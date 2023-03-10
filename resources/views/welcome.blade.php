6a6c965cbcdcd4c8f608aa997818707f7ef71bfe
fujioka <fujioka8700@gmail.com>
2023-03-10 18:48:15 +0900

    [update] 2周目 抽象クラス 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,37 +8,19 @@
   </head>
   <body>
     @php
-    class User {
-      protected $name;
-      public static $count = 0;
+    abstract class BaseUser{ //抽象クラス
+      public $name;
+      abstract public function sayHi();
+    }
 
-      public function __construct($name) {
-        $this->name = $name;
-        self::$count++;
-      }
-
-      public function sayHi() {
-        echo "Hello $this->name !";
+    class User extends BaseUser{
+      public function sayHi(){
+        echo "Hello";
       }
     }
 
-    class SpecialUser extends User {
-      public function saySpecialHi() {
-        echo "I am Special $this->name";
-      }
-
-      public function sayHi() {
-        echo "こんにちは $this->name !";
-      }
-    }
-
-    $riki = new User("りき");
-    $riki->sayHi();
-
-    $takashi = new SpecialUser("たかし");
-    $takashi->sayHi();
-
-    echo User::$count;
+    $user = new User;
+    $user->sayHi();
     @endphp
   </body>
 </html>
